0e31a0714da1550f2eab55cd8952da522ea04fab
Mulooki Samuel. <mulokisamuel87@gmail.com>
2024-09-18 17:39:27 +0300

    Add files via upload

---
+++ AlienIdentity.java
@@ -0,0 +1,25 @@
+
+import java.util.scanner;
+
+public class FirstJava{
+public static void main(string[]args){
+System.out.println("ARE YOU AN ALIEN:");
+scanner.sc = newScanner(system.in);
+// ENTER YOUR ALIEN NAME.
+system.out.println("ENTER YOUR ALIEN NAME:");
+string Alien=scanner.nextline();
+//ENTER YOUR ALIEN RESIDENCE.
+system.out.println("ENTER YOUR ALIEN RESIDENCE:");
+string residence=scanner.nextline();
+//ENTER YOUR SPACE SHIP NAME.
+system.out.println("ENTER YOUR SPACE SHIP NAME:");
+string Space=scanner.nextline();
+//ACCEPT INPUT.
+system.out.println("YOU ARE A TRUE ALIEN WELCOME................");
+system.out.println("ENTER YOUR ALIEN NAME IS:"+Alien);
+system.out.println("ENTER YOUR ALIEN RESIDENCE NAME IS:"+residence);
+stystem.out.println("ENTER YOUR SPACE SHIP NAME IS:"+Space);
+system.out.println("YOU ARE A TRUE ALIEN AND WELCOME TO THE ALIEN WORLD"+Alien)
+    scanner.close();
+}
+}
